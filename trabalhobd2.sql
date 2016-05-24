5e07d032ed4da984c218092087e423adcc058a28
matheusb982 <mbtm_133@hotmail.com>
2016-05-24 00:15:37 -0300

    Create trabalhobd2.sql

---
+++ trabalhobd2.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE contract;
+
+CREATE TYPE location AS (rua VARCHAR(10), 
+numero VARCHAR(10), complemento VARCHAR(30));
+
+CREATE TABLE provider (cnpj VARCHAR(11) PRIMARY KEY, 
+name VARCHAR(20), phone VARCHAR(8), endereco location);
+
+CREATE TABLE cliente (cod int PRIMARY KEY, name VARCHAR(20),
+phone VARCHAR(8), endereco location);
+
+CREATE TABLE person_legal (cnpj VARCHAR(11)) 
+INHERITS (cliente);
+
+CREATE TABLE person_physical (cpf VARCHAR(11)) 
+INHERITS (cliente);
+
+CREATE TABLE uniform (cod INTEGER, type VARCHAR(10),
+value INTEGER) INHERITS (provider);
+
+CREATE TABLE epi (cod INTEGER, type VARCHAR(10),
+value INTEGER) INHERITS (provider);
+
+CREATE TABLE maintenance (cod INTEGER, type VARCHAR(10),
+value INTEGER) INHERITS (provider);
