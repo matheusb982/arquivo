68498857ed96d3c86eb279e2266547ed0eb6dc93
matheusb982 <mbtm_133@hotmail.com>
2016-06-20 14:42:28 -0300

    Update migration.rb

--- migration.rb
+++ migration.rb
@@ -1,13 +1,33 @@
-class ChangeStatusToOrderLog < ActiveRecord::Migration
-  def up
-    remove_column :order_logs, :status
+def up
+    remove_column :users, :username
 
-    add_column :order_logs, :status, :integer, index: true, default: 0
+    add_column :users, :title, :string
+    add_column :users, :resume, :string
+    add_column :users, :description, :text
+    add_column :users, :website, :string
+    add_column :users, :youtube, :string
+    add_reference :users, :city, index: true, foreign_key: true
+    add_column :users, :zipcode, :string
+    add_column :users, :address, :string
+    add_column :users, :phonecell, :string
+    add_column :users, :phone, :string
+    add_column :users, :whatsapp, :string
+
   end
-  
+
   def down
-    add_column :order_logs, :status
+    add_column :users, :username
 
-    remove_column :order_logs, :status, :integer, index: true, default: 0
+    remove_column :users, :title, :string
+    remove_column :users, :resume, :string
+    remove_column :users, :description, :text
+    remove_column :users, :website, :string
+    remove_column :users, :youtube, :string
+    remove_reference :users, :city, index: true, foreign_key: true
+    remove_column :users, :zipcode, :string
+    remove_column :users, :address, :string
+    remove_column :users, :phonecell, :string
+    remove_column :users, :phone, :string
+    remove_column :users, :whatsapp, :string
+
   end
-end
