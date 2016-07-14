6cc1818ae9afd1bccc296446f9049ae0095a624c
matheusb982 <mbtm_133@hotmail.com>
2016-07-14 17:24:31 -0300

    Update bin_packing.rb

--- bin_packing.rb
+++ bin_packing.rb
@@ -1,40 +1,27 @@
+#var = cot.pop #retorna o ultimo elemento de um vetor
+#var = cot.pop #retorna o ultimo elemento de um vetor
+
 #Volume dos produtos
 item = []
-item[0] = 1876
-item[1] = 33609
-item[2] = 357643
-#Peso dos produtos
-item_width = []
-item_width[0] = 23
-item_width[1] = 29
-item_width[2] = 200
+item[0] = 10000000000
+item[1] = 12500000000
 
 #Volume limite das caixas
 box = []
-box[0] = 4374
-box[1] = 46750
-box[2] = 48976
-box[3] = 52488
-#Peso limite das caixas
-box_width = []
-box_width[0] = 34
-box_width[1] = 75
-box_width[2] = 85
-box_width[3] = 105
+box[0] = 1000
+box[1] = 1000000
+box[2] = 1000000000
 
 
 total_vol = 0
-total_width = 0
+
 
 item.each do |item|
   total_vol += item
 end
 
-item_width.each do |item_width|
-  total_width += item_width
-end
+
 puts "Total Voluma: #{total_vol}"
-puts "Total Peso: #{total_width}"
 
 aux = []
 
@@ -47,31 +34,30 @@ for i in 0..at
 end
 
 controller = total_vol
-controller_width = total_width
 
-while controller != 0 and controller_width != 0
+while controller != 0
 
-  if total_vol <= box.first and (total_width <= box_width.first or total_width < 0)
+  if total_vol <= box.first
     aux[0] += 1
   end
 
   for i in 0..at
 
-    if box[i-1] < total_vol and total_vol <= box[i] and (box_width[i-1] < total_width and total_width <= box[i] or total_width <= 0)
+    if box[i-1] < total_vol and total_vol <= box[i]
       aux[i] += 1
     end
 
   end
 
-  if box[at] < total_vol or box_width[at] < total_width
+  if box[at] < total_vol
     total_vol = total_vol - box[at]
-    total_width = total_width - box_width[at]
     aux[at] += 1
   else
     controller = 0
-    controller_width = 0
   end
 
 end
 
-puts aux
+for i in 0..at
+  puts "Caixa #{i}: #{aux[i]}"
+end
