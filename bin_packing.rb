7d0a10117f40b390b52b5ac06e3cb26182d50303
matheusb982 <mbtm_133@hotmail.com>
2016-07-14 14:06:45 -0300

    Update bin_packing.rb

--- bin_packing.rb
+++ bin_packing.rb
@@ -1,53 +1,77 @@
-var1 = []
-var1[0] = 3360
-var1[1] = 3360
-var1[2] = 6912
-var1[3] = 69128
-cont = []
-total = 0
+#Volume dos produtos
+item = []
+item[0] = 1876
+item[1] = 33609
+item[2] = 357643
+#Peso dos produtos
+item_width = []
+item_width[0] = 23
+item_width[1] = 29
+item_width[2] = 200
 
+#Volume limite das caixas
 box = []
-box[0] = 35698
-box[1] = 13982
-box[2] = 19857
+box[0] = 4374
+box[1] = 46750
+box[2] = 48976
+box[3] = 52488
+#Peso limite das caixas
+box_width = []
+box_width[0] = 34
+box_width[1] = 75
+box_width[2] = 85
+box_width[3] = 105
 
-cot = []
 
-var1.each do |var1|
-  total += var1
+total_vol = 0
+total_width = 0
+
+item.each do |item|
+  total_vol += item
 end
-puts total
 
-pes = total
+item_width.each do |item_width|
+  total_width += item_width
+end
+puts "Total Voluma: #{total_vol}"
+puts "Total Peso: #{total_width}"
 
-while pes != 0
+aux = []
 
-total = pes
-tt = box.count
+ant = box.count
+at = box.count-1
+atr = box.count-2
 
-for i in 0..tt-1
-  if total <= box[i]
-    cot[i] = cot[i]+i+1
-  else
-    cot[i] = 0
+for i in 0..at
+  aux[i] = 0
+end
+
+controller = total_vol
+controller_width = total_width
+
+while controller != 0 and controller_width != 0
+
+  if total_vol <= box.first and (total_width <= box_width.first or total_width < 0)
+    aux[0] += 1
   end
+
+  for i in 0..at
+
+    if box[i-1] < total_vol and total_vol <= box[i] and (box_width[i-1] < total_width and total_width <= box[i] or total_width <= 0)
+      aux[i] += 1
+    end
+
+  end
+
+  if box[at] < total_vol or box_width[at] < total_width
+    total_vol = total_vol - box[at]
+    total_width = total_width - box_width[at]
+    aux[at] += 1
+  else
+    controller = 0
+    controller_width = 0
+  end
+
 end
 
-if  total > box[tt-1]
-  cot[tt-1] = cot[tt-1] + tt
-  total = total - box[tt-1]
-  pes = total
-else
-  pes = 0
-end
-
-end
-
-cot.each do |cot|
-  puts cot
-end
-
-
-
-
-#var = cot.pop #retorna o ultimo elemento de um vetor
+puts aux
