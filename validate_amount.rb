e73182b62f5b92458c9cdcaa65df9de3b0d98218
matheusb982 <mbtm_133@hotmail.com>
2016-07-27 15:30:34 -0300

    Update validate_amount.rb

--- validate_amount.rb
+++ validate_amount.rb
@@ -2,13 +2,13 @@ validates_each :amount, on: :create do |record, field, value|
     user = User.find( record.user_id )
 
     if record.payment_method == 'free_credits'
-      if user.activation_balance < value
+      if user.activation_balance <= value
         record.errors.add(field, 'Você não tem saldo suficiente')
       end
     end
 
     if record.payment_method == 'credits'
-      if user.withdrawal_balance < value
+      if user.withdrawal_balance <= value
         record.errors.add(field, 'Você não tem saldo suficiente')
       end
     end
