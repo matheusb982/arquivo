28ff4926298c5fd81b69ac669d966db36afb764d
matheusb982 <mbtm_133@hotmail.com>
2016-12-21 23:20:35 -0300

    Update cep_log_lat.rb

--- cep_log_lat.rb
+++ cep_log_lat.rb
@@ -2,9 +2,12 @@ require "net/http"
 require "uri"
 require 'json'
 
+puts "Digite seu CEP:"
+cepp = gets.to_s
+
 domain = "http://www.cepaberto.com/api/v2/ceps.json"
 uri = URI(domain)
-params = {'cep' => '60533170'}
+params = {'cep' => cepp[0..7]}
 headers = { "Authorization" => "Token token=45a1781f8656f13874bdbe34ad745e12" }
 
 http = Net::HTTP.new(uri.host, uri.port)
@@ -13,8 +16,17 @@ request.set_form_data(params)
 request = Net::HTTP::Get.new(uri.path + '?' + request.body, headers)
 response = http.request(request)
 var = JSON.parse( response.body )
+
 puts "\n"
-print '/n/n Latitude: ' + var['latitude']
+print 'Latitude: ' + var['latitude']
 puts "\n"
-print '/n/n Longitude: ' + var['longitude']
+print 'Longitude: ' + var['longitude']
+puts "\n"
+print 'Bairro: ' + var['bairro']
+puts "\n"
+print 'Logradouro: ' + var['logradouro']
+puts "\n"
+print 'Cidade: ' + var['cidade']
+puts "\n"
+print 'Estado: ' + var['estado']
 puts "\n"
