a297f9c2060d89a7a7fa58fe30ffeb3a714d91f6
matheusb982 <mbtm_133@hotmail.com>
2018-02-06 21:35:05 -0300

    Update appkey_range.py

--- appkey_range.py
+++ appkey_range.py
@@ -8,13 +8,7 @@ init = '0d0339f69c6949bd41529f7c16'
 array = []
 
 for i in xrange(start, end + 1):
-    if (cont < 5):
-      x = randint(1, 15)
-      array.append(init+format(i+x, 'X'))
+    if (cont < 500):
+      x = randint(1, 500)
+      print init+format(i+x, 'X')
       cont += 1
-      if (cont == 5):
-        last = format(i+x, 'X')
-
-
-print array
-print last
