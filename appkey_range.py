74e6a9b83a95743fc1a7244f262e62c5b3bb9fee
matheusb982 <mbtm_133@hotmail.com>
2018-02-06 21:32:09 -0300

    Update appkey_range.py

--- appkey_range.py
+++ appkey_range.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
+from random import *
 
 start = 0x302041  # hex literal, gives us a regular integer
 end = 0x586471
 cont = 0
 init = '0d0339f69c6949bd41529f7c16'
+array = []
 
 for i in xrange(start, end + 1):
-    if (cont < 500):
-      print init + format(i, 'X')
+    if (cont < 5):
+      x = randint(1, 15)
+      array.append(init+format(i+x, 'X'))
       cont += 1
+      if (cont == 5):
+        last = format(i+x, 'X')
 
-print cont
+
+print array
+print last
